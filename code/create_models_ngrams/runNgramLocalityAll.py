e9cde643b2b8b0190658c878112fb925e4a1976f
Michael Hahn <mhahn29@gmail.com>
2019-08-15 14:43:52 +0800

    code

--- code/create_models_ngrams/runNgramLocalityAll.py
+++ code/create_models_ngrams/runNgramLocalityAll.py
@@ -9,18 +9,16 @@ version = "yWithMorphologySequentialStreamDropoutDev_Ngrams_Log.py"
 argumentNames = ["alpha", "gamma", "delta", "cutoff"]
 
 
+BASE_DIR = "/u/scr/mhahn/deps/memory-need-ngrams/"
+
 for language in languages:
 
-    filenames = [x for x in os.listdir("/u/scr/mhahn/deps/memory-need-ngrams/") if language in x and "GROUND" in x]
-    if len(filenames) > 5:
-      continue
 
-
-    filenames = ([x for x in os.listdir("/u/scr/mhahn/deps/memory-need-ngrams/") if x.startswith("search-"+language+"_yWithMo") and len(open("/u/scr/mhahn/deps/memory-need-ngrams/"+x, "r").read().split("\n"))>=30])
+    filenames = ([x for x in os.listdir(BASE_DIR) if x.startswith("search-"+language+"_yWithMo") and len(open(BASE_DIR+x, "r").read().split("\n"))>=30])
     if len(filenames) == 0:
        continue
 #    assert len(filenames) == 1
-    with open("/u/scr/mhahn/deps/memory-need-ngrams/"+filenames[0], "r") as inFile:
+    with open(BASE_DIR+filenames[0], "r") as inFile:
         params = next(inFile).strip().split("\t")[2:]
         assert len(params) == 4
         params[-1] = 20
@@ -28,9 +26,21 @@ for language in languages:
     for i in range(len(params)):
       params2.append("--"+argumentNames[i])
       params2.append(params[i])
+    paramsString = " ".join(params2)
 #    command = ["/u/nlp/anaconda/ubuntu_16/envs/py27-mhahn/bin/python2.7", "yHyperParamSearchGPUs_CorPost_Automated_OnlyWordForms_Slurm_Ngrams.py", language, "1", "2", "NONE", "RANDOM_BY_TYPE", "0.02", "30"]
-    for MODEL_TYPE in ["GROUND"]: #"REAL_REAL", "RANDOM_BY_TYPE", "GROUND"]:
-       for _ in range({"REAL_REAL" : 5, "RANDOM_BY_TYPE" : 20, "GROUND" : 5}[MODEL_TYPE]):
+    for MODEL_TYPE in ["GROUND", "REAL_REAL", "RANDOM_BY_TYPE"]:
+
+       filenames = [x for x in os.listdir(BASE_DIR) if language in x and MODEL_TYPE in x]
+       existing = 0
+       for name in filenames:
+           with open(BASE_DIR+name, "r") as inFile:
+               if paramsString in inFile.read():
+                   existing += 1
+
+
+       print(language, MODEL_TYPE, existing)
+       continue
+       for _ in range({"REAL_REAL" : 5, "RANDOM_BY_TYPE" : 20, "GROUND" : 5}[MODEL_TYPE] - existing):
           command = map(str,["/u/nlp/anaconda/ubuntu_16/envs/py27-mhahn/bin/python2.7", version, "--language", language, "--model", MODEL_TYPE] + params2)
           print(command)
 #          quit()
