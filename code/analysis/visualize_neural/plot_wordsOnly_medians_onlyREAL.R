4dc95703658deebab745ad2a0718df5669b1ca78
Michael Hahn <mhahn29@gmail.com>
2020-01-16 12:49:35 -0800

    code

--- code/analysis/visualize_neural/plot_wordsOnly_medians_onlyREAL.R
+++ code/analysis/visualize_neural/plot_wordsOnly_medians_onlyREAL.R
@@ -1,6 +1,6 @@
-    library(tidyr)
-    library(dplyr)
-    library(ggplot2)
+library(tidyr)
+library(dplyr)
+library(ggplot2)
 
 # Plots CIs for the quantile
 
@@ -15,8 +15,7 @@ memListenerSurpPlot_onlyWordForms_boundedVocab = function(language) {
     dataL = read.csv(paste("../../../results/raw/word-level/",language,"_decay_after_tuning_onlyWordForms_boundedVocab.tsv", sep=""), sep="\t")
     UnigramCE = mean(dataL$UnigramCE)
     data = fullData %>% filter(Language == language)
-    plot = ggplot(data, aes(x=Memory, y=UnigramCE-MedianEmpirical, fill=Type, color=Type)) + geom_line(size=2)+ theme_classic() + theme(legend.position="none") + geom_line(aes(x=Memory, y=UnigramCE-MedianLower), linetype="dashed") + geom_line(aes(x=Memory, y=UnigramCE-MedianUpper), linetype="dashed")    +  theme(axis.title.x=element_blank(),
-        axis.title.y=element_blank(), axis.text = element_text(size=20))
+    plot = ggplot(data, aes(x=Memory, y=UnigramCE-MedianEmpirical, fill=Type, color=Type)) + geom_line(size=2)+ theme_classic() + theme(legend.position="none") + geom_line(aes(x=Memory, y=UnigramCE-MedianLower), linetype="dashed") + geom_line(aes(x=Memory, y=UnigramCE-MedianUpper), linetype="dashed")    +  theme(axis.title.x=element_blank(), axis.title.y=element_blank(), axis.text = element_text(size=20))
     ggsave(plot, file=paste("figures/",language,"-listener-surprisal-memory-MEDIANS_onlyWordForms_boundedVocab_REAL.pdf", sep=""), height=4, width=4)
     return(plot)
 }
