7ed573046e3df879665be0dbc6f855e14e7f7e2f
Michael Hahn <mhahn29@gmail.com>
2020-03-29 06:39:12 -0700

    code

---
+++ code/ngram-control/create_models_ngrams/morph/Sesotho/tradeoffs/runModels_forWords_Sesotho_RandomOrder_FormsWordsGraphemes_HeldoutClip.py
@@ -0,0 +1,26 @@
+script = "forWords_Sesotho_RandomOrder_Normalized.py"
+
+PATH = "/u/scr/mhahn/deps/memory-need-ngrams-morphology-optimized/"
+
+import glob
+models_pfx = glob.glob(PATH+"optimized_Sesotho_Acqdiv_forWords_Sesotho_OptimizeOrder_FormsWordsGraphemes_Prefixes_ByType.py_*.tsv")
+models_sfx = glob.glob(PATH+"optimized_Sesotho_Acqdiv_forWords_Sesotho_OptimizeOrder_FormsWordsGraphemes_Suffixes_ByType.py_*.tsv")
+
+models_pfx.sort()
+models_sfx.sort()
+import random
+random.shuffle(models_pfx)
+random.shuffle(models_sfx)
+
+import subprocess
+
+for i in range(min(len(models_pfx), len(models_sfx))):
+    model_pfx = models_pfx[i]
+    model_sfx = models_sfx[i]
+    model_pfx = model_pfx[model_pfx.rfind("_")+1:-4]
+    model_sfx = model_sfx[model_sfx.rfind("_")+1:-4]
+    subprocess.call(["/u/nlp/anaconda/main/anaconda3/envs/py37-mhahn/bin/python", "forWords_Sesotho_RandomOrder_FormsWordsGraphemes_HeldoutClip.py", "--model_pfx", model_pfx, "--model_sfx", model_sfx])
+model_pfx = "REAL"
+model_sfx = "REAL"
+#subprocess.call(["/u/nlp/anaconda/main/anaconda3/envs/py37-mhahn/bin/python", "forWords_Sesotho_RandomOrder_FormsWordsGraphemes.py", "--model_pfx", model_pfx, "--model_sfx", model_sfx])
+
