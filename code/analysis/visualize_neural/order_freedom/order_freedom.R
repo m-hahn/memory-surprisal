89782112f82773412ceda5d2f37092da53f20d31
Michael Hahn <mhahn29@gmail.com>
2020-04-17 12:52:12 +0800

    code

--- code/analysis/visualize_neural/order_freedom/order_freedom.R
+++ code/analysis/visualize_neural/order_freedom/order_freedom.R
@@ -1,4 +1,4 @@
-fullData = read.csv("../../../results/tradeoff/listener-curve-histogram_byMem.tsv", sep="\t")
+fullData = read.csv("../../../../results/tradeoff/listener-curve-histogram_byMem.tsv", sep="\t")
     library(tidyr)
     library(dplyr)
     library(ggplot2)
@@ -7,7 +7,7 @@ fullData = read.csv("../../../results/tradeoff/listener-curve-histogram_byMem.ts
     data = data %>% mutate(MI_z = (MI-mean)/sd)
     barWidth = 0.05 #(max(data$MI) - min(data$MI))/30
 
-branching = read.csv("../../branching_entropy/branching_entropy.tsv", sep="\t")
+branching = read.csv("../../../order-freedom/branching_entropy/branching_entropy.tsv", sep="\t")
 data = merge(data, branching, by=c("Language"))
 
 dataReal = data %>% filter(Type %in% c("REAL_REAL")) %>% group_by(Language, Type) %>% summarise(MI_z=mean(MI_z), MIDiff = mean(MI-mean), BranchingEntropy=mean(BranchingEntropy)) %>% mutate(y=1)
@@ -35,6 +35,27 @@ plot = plot + geom_text(aes(label=Language), hjust=0.8, vjust=1.1)
 #plot = plot + xlim(-2.5, 0.5)
 
 ggsave(plot, file=paste("../figures/surprisal-branching-entropy-GROUND.pdf", sep=""))
+
+
+
+
+
+
+dataReal = data %>% filter(Type %in% c("REAL_REAL")) %>% group_by(Language, Type) %>% summarise(MI_z=mean(MI_z), MIDiff = mean(MI-mean), BranchingEntropy=mean(BranchingEntropy)) %>% mutate(y=1)
+#cor.test(dataReal$MI_z, dataReal$BranchingEntropy, method="spearman")
+cor.test(dataReal$MIDiff, dataReal$BranchingEntropy, method="spearman")
+plot = ggplot(dataReal, aes(x=-MIDiff, y=BranchingEntropy)) + geom_point()
+plot = plot + theme_classic() 
+plot = plot + theme(legend.position="none")   
+plot = plot + geom_text(aes(label=Language), hjust=0.8, vjust=1.1)
+plot = plot + xlab("Surprisal Difference")
+plot = plot + ylab("Branching Direction Entropy")
+plot = plot + theme(axis.title=element_text(size=30))
+plot = plot + geom_segment(data=data.frame(x=-0.0999, xend=-0.23, y=0.328, yend=0.2), aes(x=x, xend=xend, y=y, yend=yend), size=2, arrow=arrow(), color="red")
+ggsave(plot, file=paste("../figures/surprisal-branching-entropy-REAL-infostruc.pdf", sep=""), width=15, height=7)
+
+#   Language       Type       MI_z MIDiff BranchingEntropy     y
+#14 Czech          REAL_REAL 2.78  0.0999            0.328     1
 
 
 
