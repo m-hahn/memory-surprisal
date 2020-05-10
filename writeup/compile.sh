53db8653abe339148c3f793bdffb332e77a9730f
Michael Hahn <mhahn29@gmail.com>
2020-05-10 13:08:27 +0800

    code

---
+++ writeup/compile.sh
@@ -0,0 +1,10 @@
+pdflatex auc-table_MLE.tex
+pdflatex auc-table.tex
+pdflatex it-table.tex
+pdflatex langs-figure.tex
+pdflatex quantiles-table.tex
+pdflatex results-table-mle.tex
+pdflatex results-table-pcfg.tex
+pdflatex results-table.tex
+pdflatex results-table-word-level.tex
+
