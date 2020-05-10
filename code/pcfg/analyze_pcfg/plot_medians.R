5be782f88277c6ec4fe5e819338936f2af1b75bb
Michael Hahn <mhahn29@gmail.com>
2020-05-10 13:05:09 +0800

    code

--- code/pcfg/analyze_pcfg/plot_medians.R
+++ code/pcfg/analyze_pcfg/plot_medians.R
@@ -1,12 +1,15 @@
+library(tidyr)
+library(dplyr)
+library(ggplot2)
 
 # Plots medians with confidence intervals
 
 fullData = read.csv("../../../results/tradeoff/pcfg/listener-curve-ci-median.tsv", sep="\t")
 
-memListenerSurpPlot_onlyWordForms_boundedVocab = function(language) {
-    library(tidyr)
-    library(dplyr)
-    library(ggplot2)
+languages = read.csv("../../corpusSizes.tsv", sep="\t")
+languages = languages$Language
+
+for(language in languages) {
     dataL = read.csv(paste("../../../results/raw/pcfg/",language,"_pcfg_decay_after_tuning.tsv", sep=""), sep="\t")
     UnigramCE = mean(dataL$UnigramCE)
     data = fullData %>% filter(Language == language) %>% mutate(Type = factor(Type, levels = c("RANDOM_BY_TYPE", "GROUND")))
@@ -16,18 +19,10 @@ memListenerSurpPlot_onlyWordForms_boundedVocab = function(language) {
     plot = plot + theme(legend.position="none") 
     plot = plot + geom_line(data=data %>% filter(Type == "RANDOM_BY_TYPE"), aes(x=Memory, y=UnigramCE-MedianLower), linetype="dashed")
     plot = plot + geom_line(data=data %>% filter(Type == "RANDOM_BY_TYPE"), aes(x=Memory, y=UnigramCE-MedianUpper), linetype="dashed")
-    plot = plot + xlab("Memory") + ylab("Median Surprisal")
-    plot = plot + theme(text = element_text(size=30))
+#    plot = plot + xlab("Memory") + ylab("Median Surprisal")
+    plot = plot + theme(text = element_text(size=40), axis.title=element_blank())
     ggsave(plot, file=paste("figures/",language,"-listener-surprisal-memory-MEDIANS_onlyWordForms_boundedVocab-pcfg.pdf", sep=""))
-    return(plot)
 }
 
-
-languages = read.csv("../../corpusSizes.tsv", sep="\t")
-languages = languages$Language
-
-for(language in languages) {
-   memListenerSurpPlot_onlyWordForms_boundedVocab(language)
-}
 
 
