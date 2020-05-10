bdf1589b541a9df5484dbac39f6ac903ac30195a
Michael Hahn <mhahn29@gmail.com>
2020-05-10 14:44:43 +0800

    code

--- code/study3/Sesotho/tradeoffs/analyze_Morphemic_Heldout.R
+++ code/study3/Sesotho/tradeoffs/analyze_Morphemic_Heldout.R
@@ -28,12 +28,29 @@ data_ = data %>% filter(Type %in% c("Real", "Random", "Optimized", "Reverse"))
 plot = ggplot(data_, aes(x=AUC, fill=Type, color=Type))
 plot = plot + theme_classic()
 plot = plot + xlab("Area under Curve") + ylab("Density")
-#plot = plot + theme(legend.position="none")
+plot = plot + theme(text=element_text(size=30))
 plot = plot + geom_density(data= data_%>%filter(Type == "Random"), aes(y=..scaled..)) 
-plot = plot + geom_bar(data = data_ %>% filter(Language=="Japanese", !(Type %in% c("Random"))) %>% group_by(Language, Type) %>% summarise(AUC=mean(AUC), barWidth=mean(barWidth)) %>% mutate(y=1),  aes(y=y, group=Type), width=0.02, stat="identity", position = position_dodge())
-plot = plot + geom_bar(data = data_ %>% filter(Language=="Sesotho", !(Type %in% c("Random"))) %>% group_by(Language, Type) %>% summarise(AUC=mean(AUC), barWidth=mean(barWidth)) %>% mutate(y=1),  aes(y=y, group=Type), width=0.01, stat="identity", position = position_dodge())
+plot = plot + geom_bar(data = data_ %>% filter(Language=="Japanese", !(Type %in% c("Random"))) %>% group_by(Language, Type) %>% summarise(AUC=mean(AUC), barWidth=0.1) %>% mutate(y=1),  aes(y=y, group=Type), width=0.02, stat="identity", position = position_dodge())
+plot = plot + geom_bar(data = data_ %>% filter(Language=="Sesotho", !(Type %in% c("Random"))) %>% group_by(Language, Type) %>% summarise(AUC=mean(AUC), barWidth=0.1) %>% mutate(y=1),  aes(y=y, group=Type), width=0.01, stat="identity", position = position_dodge())
+plot = plot + facet_wrap(~Language, scales = "free")
+#ggsave(plot, file=paste("figures/Both-suffixes-byMorphemes-auc-hist-heldout.pdf", sep=""), height=4, width=12)
+
+# Version with SD
+plot = ggplot(data_, aes(x=AUC, fill=Type, color=Type))
+plot = plot + theme_classic()
+plot = plot + xlab("Area under Curve") + ylab("Density")
+plot = plot + theme(text=element_text(size=30))
+plot = plot + geom_density(data= data_%>%filter(Type == "Random"), aes(y=..scaled..)) 
+plot = plot + geom_bar(data = data_ %>% filter(Language=="Japanese", !(Type %in% c("Random"))) %>% group_by(Language, Type) %>% summarise(AUC=mean(AUC), barWidth=0.08) %>% mutate(y=1),  aes(y=y, group=Type), width=0.02, stat="identity", position = position_dodge())
+plot = plot + geom_errorbarh(data = data_ %>% filter(Language=="Japanese", (Type %in% c("Optimized"))) %>% group_by(Language, Type) %>% summarise(AUC_min=min(AUC), AUC_max=max(AUC), AUC_sd=sd(AUC), AUC=mean(AUC), barWidth=0.1) %>% mutate(y=0.5), aes(y=y, xmin=AUC_min, xmax=AUC_max))
+plot = plot + geom_bar(data = data_ %>% filter(Language=="Sesotho", !(Type %in% c("Random"))) %>% group_by(Language, Type) %>% summarise(AUC=mean(AUC), barWidth=0.08) %>% mutate(y=1),  aes(y=y, group=Type), width=0.01, stat="identity", position = position_dodge())
+plot = plot + geom_errorbarh(data = data_ %>% filter(Language=="Sesotho", (Type %in% c("Optimized"))) %>% group_by(Language, Type) %>% summarise(AUC_min=min(AUC), AUC_max=max(AUC), AUC_sd=sd(AUC), AUC=mean(AUC), barWidth=0.1) %>% mutate(y=0.5), aes(y=y, xmin=AUC_min, xmax=AUC_max))
 plot = plot + facet_wrap(~Language, scales = "free")
 ggsave(plot, file=paste("figures/Both-suffixes-byMorphemes-auc-hist-heldout.pdf", sep=""), height=4, width=12)
+
+
+
+
 
 
 
