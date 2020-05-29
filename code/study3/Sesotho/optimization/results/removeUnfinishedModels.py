ff12f7d63371810e7e90c3e5b5fccd7fb2ef5ea9
Michael Hahn <mhahn29@gmail.com>
2020-05-29 16:42:01 -0700

    code

--- code/study3/Sesotho/optimization/results/removeUnfinishedModels.py
+++ code/study3/Sesotho/optimization/results/removeUnfinishedModels.py
@@ -9,7 +9,7 @@ for dir in dirs:
   for filename in files:
     with open(dir+"/"+filename, "r") as inFile:
       iterations = int(next(inFile).strip().split(" ")[0])
-    if iterations == 999:
+    if iterations == 999 or (("Graphemes" in filename or "Heldout" not in filename) and iterations > 400):
        foundModels += 1
     else:
        os.remove(dir+"/"+filename)
