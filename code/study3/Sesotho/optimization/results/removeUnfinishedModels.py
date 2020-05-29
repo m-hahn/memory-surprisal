7c5e963abdb6bcb84066bdcd728d0b89046cea58
Michael Hahn <mhahn29@gmail.com>
2020-05-28 18:33:25 -0700

    code

---
+++ code/study3/Sesotho/optimization/results/removeUnfinishedModels.py
@@ -0,0 +1,18 @@
+import os
+dirs = os.listdir(".")
+for dir in dirs:
+  try:
+     files = os.listdir(dir+"/")
+  except NotADirectoryError:
+     continue
+  foundModels = 0
+  for filename in files:
+    with open(dir+"/"+filename, "r") as inFile:
+      iterations = int(next(inFile).strip().split(" ")[0])
+    if iterations == 999:
+       foundModels += 1
+    else:
+       os.remove(dir+"/"+filename)
+ #   print(iterations)
+  print(dir, foundModels)
+
