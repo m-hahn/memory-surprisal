d01f22ed124ae292cb74523e17ca791e01566037
Michael Hahn <mhahn29@gmail.com>
2020-06-01 11:35:15 +0800

    code

--- code/study3/Sesotho/tradeoffs/analyze/analyze_Morphemic_Heldout.R
+++ code/study3/Sesotho/tradeoffs/analyze/analyze_Morphemic_Heldout.R
@@ -4,18 +4,53 @@ library(ggplot2)
 
 ######################################
 
-dataSesotho = read.csv("results_auc.tsv", sep="\t")
-dataSesotho = dataSesotho %>% filter(Script == "forWords_Sesotho_RandomOrder_Normalized_HeldoutClip.py") %>% mutate(Language="Sesotho")
+data = read.csv("results.tsv", sep="\t")
+data = data %>% filter(Script == "forWords_Sesotho_RandomOrder_Normalized_HeldoutClip.py")
+
+unigramCE = mean(data$UnigramCE)
 
 
-dataJapanese = read.csv("../../../Japanese/tradeoffs/analyze/results_auc.tsv", sep="\t")
-dataJapanese = dataJapanese %>% filter(Script == "forWords_Japanese_RandomOrder_Normalized_FullData_Heldout.py") %>% mutate(Language="Japanese")
+data_ = data %>% group_by(Distance, Type) %>% summarise(MI=median(MI))
+data_ = data_ %>% filter(Distance <= 7)
+plot = ggplot(data_, aes(x=1+Distance, y=MI, color=Type)) + geom_line() #size=2)
+plot = plot + theme_bw()
+plot = plot + xlab("Distance") + ylab("Mutual Information")
+plot = plot + theme(axis.title.x=element_text(size=20), axis.title.y=element_text(size=20), axis.text = element_text(size=20))
+plot = plot + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), panel.background = element_blank(), legend.position="none")
+plot = plot + theme(axis.line = element_line(colour = "black"),
+            panel.grid.major = element_blank(),
+                panel.grid.minor = element_blank(),
+                panel.border = element_blank(),
+                    panel.background = element_blank())
+ggsave(plot, file=paste("../figures/Sesotho-suffixes-byMorphemes-it-heldout.pdf", sep=""), height=4, width=4)
 
-#dataJapanese$barWidth = (max(dataJapanese$AUC) - min(dataJapanese$AUC))/5
-#dataSesotho$barWidth =(max(dataSesotho$AUC) - min(dataSesotho$AUC))/5
 
 
-data = rbind(dataJapanese, dataSesotho)
+
+data = read.csv("results_interpolated.tsv", sep="\t")
+
+
+data = data %>% filter(Script == "forWords_Sesotho_RandomOrder_Normalized_HeldoutClip.py")
+
+data = data %>% group_by(Type, Memory) %>% summarise(Surprisal=unigramCE-median(MI))
+
+plot = ggplot(data, aes(x=Memory, y=Surprisal, color=Type)) + geom_line() #size=2)
+plot = plot + theme_bw()
+plot = plot + theme(axis.title.x=element_text(size=20), axis.title.y=element_text(size=20), axis.text = element_text(size=20))
+plot = plot + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), panel.background = element_blank(), legend.position="none")
+plot = plot + theme(axis.line = element_line(colour = "black"),
+            panel.grid.major = element_blank(),
+                panel.grid.minor = element_blank(),
+                panel.border = element_blank(),
+                    panel.background = element_blank())
+ggsave(plot, file=paste("../figures/Sesotho-suffixes-byMorphemes-memsurp-heldout.pdf", sep=""), height=4, width=4)
+
+
+######################################
+
+data = read.csv("results_auc.tsv", sep="\t")
+data = data %>% filter(Script == "forWords_Sesotho_RandomOrder_Normalized_HeldoutClip.py")
+
 
 data$Type = ifelse(data$Model %in% c("REAL", "RANDOM", "REVERSE"), as.character(data$Model), "Optimized")
 data$Type = ifelse(data$Type %in% c("REAL"), "Real", as.character(data$Type))
@@ -24,47 +59,11 @@ data$Type = ifelse(data$Type %in% c("REVERSE"), "Reverse", as.character(data$Typ
 
 data_ = data %>% filter(Type %in% c("Real", "Random", "Optimized", "Reverse"))
 
-
 plot = ggplot(data_, aes(x=AUC, fill=Type, color=Type))
 plot = plot + theme_classic()
 plot = plot + xlab("Area under Curve") + ylab("Density")
 plot = plot + theme(text=element_text(size=30))
 plot = plot + geom_density(data= data_%>%filter(Type == "Random"), aes(y=..scaled..)) 
-plot = plot + geom_bar(data = data_ %>% filter(Language=="Japanese", !(Type %in% c("Random"))) %>% group_by(Language, Type) %>% summarise(AUC=mean(AUC), barWidth=0.1) %>% mutate(y=1),  aes(y=y, group=Type), width=0.02, stat="identity", position = position_dodge())
-plot = plot + geom_bar(data = data_ %>% filter(Language=="Sesotho", !(Type %in% c("Random"))) %>% group_by(Language, Type) %>% summarise(AUC=mean(AUC), barWidth=0.1) %>% mutate(y=1),  aes(y=y, group=Type), width=0.01, stat="identity", position = position_dodge())
-plot = plot + facet_wrap(~Language, scales = "free")
-#ggsave(plot, file=paste("figures/Both-suffixes-byMorphemes-auc-hist-heldout.pdf", sep=""), height=4, width=12)
-
-# Version with SD
-plot = ggplot(data_, aes(x=AUC, fill=Type, color=Type))
-plot = plot + theme_classic()
-plot = plot + xlab("Area under Curve") + ylab("Density")
-plot = plot + theme(text=element_text(size=30))
-plot = plot + geom_density(data= data_%>%filter(Type == "Random"), aes(y=..scaled..)) 
-plot = plot + geom_bar(data = data_ %>% filter(Language=="Japanese", !(Type %in% c("Random"))) %>% group_by(Language, Type) %>% summarise(AUC=mean(AUC), barWidth=0.08) %>% mutate(y=1),  aes(y=y, group=Type), width=0.02, stat="identity", position = position_dodge())
-plot = plot + geom_errorbarh(data = data_ %>% filter(Language=="Japanese", (Type %in% c("Optimized"))) %>% group_by(Language, Type) %>% summarise(AUC_min=min(AUC), AUC_max=max(AUC), AUC_sd=sd(AUC), AUC=mean(AUC), barWidth=0.1) %>% mutate(y=0.5), aes(y=y, xmin=AUC_min, xmax=AUC_max))
-plot = plot + geom_bar(data = data_ %>% filter(Language=="Sesotho", !(Type %in% c("Random"))) %>% group_by(Language, Type) %>% summarise(AUC=mean(AUC), barWidth=0.08) %>% mutate(y=1),  aes(y=y, group=Type), width=0.01, stat="identity", position = position_dodge())
-plot = plot + geom_errorbarh(data = data_ %>% filter(Language=="Sesotho", (Type %in% c("Optimized"))) %>% group_by(Language, Type) %>% summarise(AUC_min=min(AUC), AUC_max=max(AUC), AUC_sd=sd(AUC), AUC=mean(AUC), barWidth=0.1) %>% mutate(y=0.5), aes(y=y, xmin=AUC_min, xmax=AUC_max))
-plot = plot + facet_wrap(~Language, scales = "free")
-ggsave(plot, file=paste("../figures/Both-suffixes-byMorphemes-auc-hist-heldout.pdf", sep=""), height=4, width=12)
-
-
-
-
-
-
-
-1-mean(((data_ %>% filter(Language=="Japanese", (Type %in% c("Random"))))$AUC) < max(((data_ %>% filter(Language=="Japanese", (Type %in% c("Optimized"))))$AUC)))
-1-mean(((data_ %>% filter(Language=="Japanese", (Type %in% c("Random"))))$AUC) < max(((data_ %>% filter(Language=="Japanese", (Type %in% c("Real"))))$AUC)))
-1-mean(((data_ %>% filter(Language=="Sesotho", (Type %in% c("Random"))))$AUC) < max(((data_ %>% filter(Language=="Sesotho", (Type %in% c("Optimized"))))$AUC)))
-1-mean(((data_ %>% filter(Language=="Sesotho", (Type %in% c("Random"))))$AUC) < max(((data_ %>% filter(Language=="Sesotho", (Type %in% c("Real"))))$AUC)))
-
-# Variation in AUC for Optimized Grammars
-sd(((data_ %>% filter(Language=="Japanese", (Type %in% c("Optimized"))))$AUC))
-
-
-# Variation in AUC for Optimized Grammars
-sd(((data_ %>% filter(Language=="Sesotho", (Type %in% c("Optimized"))))$AUC))
-
-
+plot = plot + geom_bar(data = data_ %>% filter(!(Type %in% c("Random"))) %>% group_by(Type) %>% summarise(AUC=mean(AUC), barWidth=0.1) %>% mutate(y=1),  aes(y=y, group=Type), width=0.01, stat="identity", position = position_dodge())
+ggsave(plot, file=paste("../figures/Sesotho-suffixes-byMorphemes-it-heldout.pdf", sep=""), height=4, width=4)
 
--- code/study3/Sesotho/tradeoffs/analyze/analyze_Phonemic_Heldout.R
+++ code/study3/Sesotho/tradeoffs/analyze/analyze_Phonemic_Heldout.R
@@ -5,13 +5,14 @@ library(ggplot2)
 ######################################
 
 data = read.csv("results.tsv", sep="\t")
-
-
 data = data %>% filter(Script == "forWords_Sesotho_RandomOrder_FormsWordsGraphemes_HeldoutClip.py")
 
 unigramCE = mean(data$UnigramCE)
 
 data$Type = ifelse(data$Model %in% c("REAL", "RANDOM", "REVERSE"), as.character(data$Model), "Optimized")
+data$Type = ifelse(data$Type %in% c("REAL"), "Real", as.character(data$Type))
+data$Type = ifelse(data$Type %in% c("RANDOM"), "Random", as.character(data$Type))
+data$Type = ifelse(data$Type %in% c("REVERSE"), "Reverse", as.character(data$Type))
 
 data_ = data %>% group_by(Distance, Type) %>% summarise(MI=median(MI))
 data_ = data_ %>% filter(Distance <= 7)
@@ -55,7 +56,6 @@ ggsave(plot, file=paste("../figures/Sesotho-suffixes-byPhonemes-memsurp-heldout.
 ######################################
 
 data = read.csv("results_auc.tsv", sep="\t")
-
 data = data %>% filter(Script == "forWords_Sesotho_RandomOrder_FormsWordsGraphemes_HeldoutClip.py")
 
 
@@ -82,7 +82,8 @@ barWidth = (max(data$AUC) - min(data$AUC))/30
 
 plot = ggplot(data_, aes(x=AUC, fill=Type, color=Type))
 plot = plot + theme_classic()
-#plot = plot + theme(legend.position="none")
+plot = plot + xlab("Area under Curve") + ylab("Density")
+plot = plot + theme(text=element_text(size=30))
 plot = plot + geom_density(data= data_%>%filter(Type == "Random"), aes(y=..scaled..)) 
 plot = plot + geom_bar(data = data_ %>% filter(!(Type %in% c("Random"))) %>% group_by(Type) %>% summarise(AUC=mean(AUC)) %>% mutate(y=1),  aes(y=y, group=Type), width=barWidth, stat="identity", position = position_dodge())
 ggsave(plot, file=paste("../figures/Sesotho-suffixes-byPhonemes-auc-hist-heldout.pdf", sep=""), height=4, width=4)
