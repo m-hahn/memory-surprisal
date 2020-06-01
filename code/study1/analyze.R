6a4ba3d83737d291ae0a5182b59b0e78b9ef6aad
Michael Hahn <mhahn29@gmail.com>
2020-06-01 08:56:59 +0800

    code

--- code/study1/analyze.R
+++ code/study1/analyze.R
@@ -2,16 +2,24 @@ library(tidyr)
 library(dplyr)
 library(ggplot2)
 
-data = read.csv("results.tsv", sep="\t")
+data1 = read.csv("results-simulation-6-short.py.tsv", sep="\t") %>% mutate(Order = "Short")
+data2 = read.csv("results-simulation-6-long.py.tsv", sep="\t") %>% mutate(Order = "Long")
+data=rbind(data1, data2)
 
-data = data %>% mutate(Order = ifelse(Type=="good", "A", "B"))
 
 
-data = data %>% mutate(Distance_ = ifelse(Order == "A" & Distance == 1.0, 1.3, Distance))
+data = data %>% mutate(Distance_ = ifelse(Order == "Short" & Distance == 1.0, 1.3, Distance)) %>% filter(Distance<7)
+
+#plot = ggplot(data=data, aes(x=Distance, y=MI, color=Order)) + 
+#	geom_step(data=data, size=2) + 
+#	theme_bw() + 
+#	theme(axis.text=element_text(size=18), axis.title=element_text(size=18), legend.title=element_text(size=18), legend.text=element_text(size=18)) +
+#	xlab("t") + ylab("Conditional Mutual Information (It)")
+#
 
 plot = ggplot(data=data, aes(x=Distance_, y=MI, color=Order)) + 
-	geom_step(data=data %>% filter(Order == "A"), size=2, linetype=2) + 
-	geom_step(data=data %>% filter(Order == "B"), size=2, linetype=2) + 
+	geom_step(data=data %>% filter(Order == "Short"), size=2, linetype=2) + 
+	geom_step(data=data %>% filter(Order == "Long"), size=2, linetype=2) + 
 	geom_step(data=data %>% filter(Distance_==2 | Distance_==3), size=2, linetype=1) + 
 	geom_step(data=data %>% filter(Distance_==5 | Distance_==6), size=2, linetype=1) + 
 	theme_bw() + 
@@ -36,21 +44,22 @@ ggsave("figures/toy-t-mis.pdf")
 
 
 data$MI[is.na(data$MI)] = 0
-data = data %>% group_by(Order) %>% mutate(CumMI = cumsum(MI), Memory = cumsum(Distance*MI)) %>% mutate(Surprisal = 2.07-CumMI)
+data = data %>% group_by(Order) %>% mutate(CumMI = cumsum(MI), Memory = cumsum(Distance*MI)) %>% mutate(Surprisal = 1.889-CumMI)
 #plot = ggplot(data=data %>% filter(Surprisal < 2.0), aes(x=Surprisal, y=Memory, color=Order)) + geom_line(size=2) + theme_bw() + theme(axis.text=element_text(size=18), axis.title=element_text(size=18), legend.title=element_text(size=18), legend.text=element_text(size=18))
 #ggsave("figures/toy-surp-mem.pdf")
 
-data = data %>% mutate(Surprisal_ = ifelse(Order == "A" & Memory < 2, 0.285, Surprisal))
-data = data %>% mutate(Memory_ = ifelse(Order == "A" & Memory < 2, 1.79, Memory))
+data = data %>% mutate(Surprisal_ = Surprisal) #= ifelse(Order == "Short" & Memory < 2, 0.285, Surprisal))
+data = data %>% mutate(Memory_ = Memory) #ifelse(Order == "Short" & Memory < 2, 1.79, Memory))
 
 
 data_ = data %>% select(Surprisal_, Memory_, Order)
 
-data_ = rbind(as.data.frame(data_), data.frame(Memory_=c(2.07, 2.433962), Surprisal_=c(0.146, 0.07441896), Order=c("A", "A")))
+data_ = rbind(as.data.frame(data_), data.frame(Memory_=c(2.12, 2.35), Surprisal_=c(0.077, 0.077), Order=c("Short", "Short")))
 
-plot = ggplot(data=data_ %>% filter(Surprisal_ < 2.0), aes(x=Memory_, y=Surprisal_, color=Order)) + 
+
+plot = ggplot(data=data_ %>% filter(Surprisal_ < 1.0), aes(x=Memory_, y=Surprisal_, color=Order)) + 
 	geom_line(size=2, linetype=2) + 
-	geom_line(data=data_%>%filter(Memory_>2.0), size=2, linetype=1) + 
+	geom_line(data=data_%>%filter(Memory_>1.7), size=2, linetype=1) + 
 	theme_bw() + 
 	theme(axis.text=element_text(size=18), axis.title=element_text(size=18), legend.title=element_text(size=18), legend.text=element_text(size=18)) +
 	xlab("Memory") + ylab("Surprisal")
