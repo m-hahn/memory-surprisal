a71c370bcc0fb784b8bdfc674da373e0af31ffeb
Michael Hahn <mhahn29@gmail.com>
2020-07-31 21:17:08 -0700

    code

--- code/create_models/fractions/medianCI_All.py
+++ code/create_models/fractions/medianCI_All.py
@@ -1,10 +1,10 @@
 import sys
 trainingSize = sys.argv[1]
+language = sys.argv[2]
 import subprocess
 
 # ./python27 yStudyTradeoff_Bootstrap_Parallel_OnlyWordForms_BoundedVocab_HistogramsByMem_All.py > ../results/tradeoff/listener-curve-histogram_byMem.tsv
-language = "Czech"
-with open("results/tradeoff/listener-curve-ci-median_"+trainingSize+".tsv", "w") as outFile:
+with open("results/tradeoff/"+language+"_listener-curve-ci-median_"+trainingSize+".tsv", "w") as outFile:
   print >> outFile, "\t".join(["Language", "Type", "Position", "Memory", "MedianEmpirical", "MedianLower", "MedianUpper", "Level"])
   if True:
      print(language)
--- code/create_models/fractions/runAll.py
+++ code/create_models/fractions/runAll.py
@@ -1,5 +1,7 @@
+import sys
 import subprocess
+language=sys.argv[1]
 for trainingSize in [500, 1000, 2000, 5000, 10000, 20000]:
-   subprocess.call(["./python27", "yAnalyzeNeuralMorphFinalSaveLast_OnlyWordForms_BoundedVocab_Adjusted.py", "--trainingSize="+str(trainingSize)])
-   subprocess.call(["./python27", "medianCI_All.py", str(trainingSize)])
+   subprocess.call(["./python27", "yAnalyzeNeuralMorphFinalSaveLast_OnlyWordForms_BoundedVocab_Adjusted.py", "--trainingSize="+str(trainingSize), "--language="+language])
+   subprocess.call(["./python27", "medianCI_All.py", str(trainingSize), language])
 
