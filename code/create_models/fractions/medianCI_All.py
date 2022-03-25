f34ef6d7014e1be1d365bf26f0e6af295226de20
Michael Hahn <mhahn29@gmail.com>
2022-03-24 21:10:05 -0700

    code

--- code/create_models/fractions/medianCI_All.py
+++ code/create_models/fractions/medianCI_All.py
@@ -2,8 +2,8 @@ import sys
 import subprocess
 
 # ./python27 yStudyTradeoff_Bootstrap_Parallel_OnlyWordForms_BoundedVocab_HistogramsByMem_All.py > ../results/tradeoff/listener-curve-histogram_byMem.tsv
-for language in ["Czech", "English", "Spanish", "Russian"]:
- for trainingSize in ["500", "2000"]:
+for language in ["Czech"]: #, "English", "Spanish", "Russian"]:
+ for trainingSize in ["500", "1000", "2000", "5000", "10000"]:
   with open("results/tradeoff/"+language+"_listener-curve-ci-median_"+trainingSize+".tsv", "w") as outFile:
    print >> outFile, "\t".join(["Language", "Type", "Position", "Memory", "MedianEmpirical", "MedianLower", "MedianUpper", "Level"])
    if True:
